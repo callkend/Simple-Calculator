9d3f76008e5fb7dee8739c56d675c5901ce7f605
callkend <77285430+callkend@users.noreply.github.com>
2021-01-20 08:45:20 -0700

    Add project files.

---
+++ SimpleCalculator.vb
@@ -0,0 +1,33 @@
+﻿'Kendall Callister
+'Tim Rossiter
+'RCET0265
+'Fall 2020
+'Simple Calculator
+
+
+
+Module SimpleCalculator
+
+    Sub Main()
+        Dim numberOne As Decimal
+        Dim numberTwo As Decimal
+        Dim sign As Int16
+
+
+        Console.WriteLine("Number One=")
+        numberOne = Console.ReadLine()
+        Console.WriteLine("Number Two=")
+        numberTwo = Console.ReadLine
+        Console.WriteLine("Choose Option")
+        Console.WriteLine("1. Sum")
+        Console.WriteLine("2. Product")
+        sign = Console.ReadLine()
+        If sign = 1 Then
+            Console.WriteLine(numberOne + numberTwo)
+        ElseIf sign = 2 Then
+            Console.WriteLine(numberOne * numberTwo)
+        End If
+        Console.Read()
+    End Sub
+
+End Module
