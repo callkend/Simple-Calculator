a1e6245eef4215b4be7822c6ec16bc4a8b511d63
callkend <77285430+callkend@users.noreply.github.com>
2021-01-21 08:17:29 -0700

    Changed how the operator is selected.

--- SimpleCalculator.vb
+++ SimpleCalculator.vb
@@ -5,26 +5,29 @@
 'Simple Calculator
 'https://github.com/callkend/Simple-Calculator.git
 
+Option Explicit On
+Option Strict On
+
 
 Module SimpleCalculator
 
     Sub Main()
         Dim numberOne As Decimal
         Dim numberTwo As Decimal
-        Dim sign As Int16
+        Dim sign As String
 
 
         Console.WriteLine("Number One=")
-        numberOne = Console.ReadLine()
+        numberOne = CDec(Console.ReadLine())
         Console.WriteLine("Number Two=")
-        numberTwo = Console.ReadLine
+        numberTwo = CDec(Console.ReadLine())
         Console.WriteLine("Choose Option")
         Console.WriteLine("1. Sum")
         Console.WriteLine("2. Product")
         sign = Console.ReadLine()
-        If sign = 1 Then
+        If sign = "1" Or sign = "1." Or sign = "Sum" Then
             Console.WriteLine(numberOne + numberTwo)
-        ElseIf sign = 2 Then
+        ElseIf sign = "2" Or sign = "2." Or sign = "Product" Then
             Console.WriteLine(numberOne * numberTwo)
         End If
         Console.Read()
