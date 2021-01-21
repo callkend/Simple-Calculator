56e77a6e36b56484a91317cdd3fbb70e26c322bf
callkend <77285430+callkend@users.noreply.github.com>
2021-01-21 09:03:59 -0700

    Added an exeption catch

--- SimpleCalculator.vb
+++ SimpleCalculator.vb
@@ -12,25 +12,46 @@ Option Strict On
 Module SimpleCalculator
 
     Sub Main()
+        Dim userInput As String
         Dim numberOne As Decimal
         Dim numberTwo As Decimal
         Dim sign As String
+        Dim problem As Boolean
 
 
         Console.WriteLine("Number One=")
-        numberOne = CDec(Console.ReadLine())
+        Try
+            userInput = Console.ReadLine()
+            numberOne = CDec(userInput)
+        Catch e As Exception
+            Console.WriteLine($"{userInput} is a invaild input")
+            problem = True
+        End Try
+
         Console.WriteLine("Number Two=")
-        numberTwo = CDec(Console.ReadLine())
+
+        Try
+            userInput = Console.ReadLine()
+            numberTwo = CDec(userInput)
+        Catch e As Exception
+            Console.WriteLine($"{userInput} is a invaild input")
+            problem = True
+        End Try
+
         Console.WriteLine("Choose Option")
-        Console.WriteLine("1. Sum")
-        Console.WriteLine("2. Product")
-        sign = Console.ReadLine()
-        If sign = "1" Or sign = "1." Or sign = "Sum" Then
-            Console.WriteLine(numberOne + numberTwo)
-        ElseIf sign = "2" Or sign = "2." Or sign = "Product" Then
-            Console.WriteLine(numberOne * numberTwo)
+            Console.WriteLine("1. Sum")
+            Console.WriteLine("2. Product")
+            sign = Console.ReadLine()
+
+        If problem = False And sign = "1" Or sign = "1." Or sign = "Sum" Then
+            Console.WriteLine($"{numberOne} + {numberTwo} = {numberOne + numberTwo}")
+        ElseIf problem = False And sign = "2" Or sign = "2." Or sign = "Product" Then
+            Console.WriteLine($"{numberOne} * {numberTwo} = {numberOne * numberTwo}")
+        Else
+            Console.WriteLine("Invaild input try later.")
         End If
-        Console.Read()
+
+            Console.Read()
     End Sub
 
 End Module
